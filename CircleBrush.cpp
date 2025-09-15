b2ebdb92765f11f7735833194c32669bb9c7491b
Bondjono19 <144017805+Bondjono19@users.noreply.github.com>
2025-09-15 19:24:32 +0800

    Add 2 brushes; scattered circles & circles

---
+++ CircleBrush.cpp
@@ -0,0 +1,63 @@
+// CircleBrush.cpp
+
+#include "impressionistDoc.h"
+#include "impressionistUI.h"
+#include "circlebrush.h"
+#include "math.h"
+#include "vector"
+
+extern float frand();
+
+CircleBrush::CircleBrush(ImpressionistDoc* pDoc, char* name) :
+	ImpBrush(pDoc, name)
+{
+}
+
+void CircleBrush::BrushBegin(const Point source, const Point target)
+{
+	ImpressionistDoc* pDoc = GetDocument();
+	ImpressionistUI* dlg = pDoc->m_pUI;
+
+	int size = pDoc->getSize();
+
+	glPointSize((float)size);
+
+	BrushMove(source, target);
+}
+
+std::vector<float> calculateCirclePoints(int radius, int segments, const Point target) {
+	std::vector<float> circleVertices;
+	circleVertices.reserve(segments * 2);
+	for (int i = 0;i < segments;i++) {
+		float ii = (2 * M_PI) * float(float(i) / segments);
+		float x = target.x + cosf(ii) * radius;
+		float y = target.y + sinf(ii) * radius;
+		circleVertices.push_back(x);
+		circleVertices.push_back(y);
+	}
+	return circleVertices;
+}
+
+void CircleBrush::BrushMove(const Point source, const Point target)
+{
+	ImpressionistDoc* pDoc = GetDocument();
+	ImpressionistUI* dlg = pDoc->m_pUI;
+
+	if (pDoc == NULL) {
+		printf("CircleBrush::BrushMove  document is NULL\n");
+		return;
+	}
+	int size = pDoc->getSize();
+	glBegin(GL_TRIANGLE_FAN);
+	SetColor(source);
+	std::vector<float> vertices = calculateCirclePoints(size, 100, target);
+	for (int i = 0; i < vertices.size(); i=i+2) {
+		glVertex2d(vertices[i], vertices[i + 1]);
+	}
+	glEnd();
+}
+
+void CircleBrush::BrushEnd(const Point source, const Point target)
+{
+	// do nothing so far
+}
--- ImpressionistDoc.cpp
+++ ImpressionistDoc.cpp
@@ -14,7 +14,8 @@
 
 // Include individual brush headers here.
 #include "PointBrush.h"
-
+#include "CircleBrush.h"
+#include "ScatteredCircleBrush.h"
 
 #define DESTROY(p)	{  if ((p)!=NULL) {delete [] p; p=NULL; } }
 
@@ -38,13 +39,13 @@ ImpressionistDoc::ImpressionistDoc()
 	ImpBrush::c_pBrushes[BRUSH_LINES]				
 		= new PointBrush( this, "Lines" );
 	ImpBrush::c_pBrushes[BRUSH_CIRCLES]				
-		= new PointBrush( this, "Circles" );
+		= new CircleBrush( this, "Circles" );
 	ImpBrush::c_pBrushes[BRUSH_SCATTERED_POINTS]	
 		= new PointBrush( this, "Scattered Points" );
 	ImpBrush::c_pBrushes[BRUSH_SCATTERED_LINES]		
 		= new PointBrush( this, "Scattered Lines" );
 	ImpBrush::c_pBrushes[BRUSH_SCATTERED_CIRCLES]	
-		= new PointBrush( this, "Scattered Circles" );
+		= new ScatteredCircleBrush( this, "Scattered Circles" );
 
 	// make one of the brushes current
 	m_pCurrentBrush	= ImpBrush::c_pBrushes[0];
